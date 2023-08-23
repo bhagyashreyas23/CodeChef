0bd53ecfce3211e6148d642dcd330b725cde2408
Bhagya Shreyas Tripathi <124259707+bhagyashreyas23@users.noreply.github.com>
2023-08-23 17:05:13 +0530

    Create goodturn.cpp

---
+++ C++/goodturn.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        int y;
+        cin >> x >> y;
+        if ((x + y) > 6)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
